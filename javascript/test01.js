ed316a803e93d5729dcd1c9a06d240a9ce30b3cb
liyaqing <ex_wljr_liyaqing@pingan.com.cn>
2018-08-23 16:52:58 +0800

    js练习

---
+++ javascript/test01.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function foo() {
+    
+    for(var i=0;i<100;i++){
+      console.log('i='+i);
+        
+    }
+    i = -1;
+    alert('out i'+i);
+}
+
+// foo(); // 直接调用foo()
+// window.foo(); 
