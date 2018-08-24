a91214085fadd1fee844c4840d63d71abe8cdb4b
liyaqing <ex_wljr_liyaqing@pingan.com.cn>
2018-08-24 17:09:35 +0800

    map

--- javascript/test01.js
+++ javascript/test01.js
@@ -1,14 +1,31 @@
 'use strict';
 
 function foo() {
-    
-    for(var i=0;i<100;i++){
-      console.log('i='+i);
-        
-    }
-    i = -1;
-    alert('out i'+i);
+
+  for (var i = 0; i < 100; i++) {
+    console.log('i=' + i);
+
+  }
+  i = -1;
+  alert('out i' + i);
 }
 
 // foo(); // 直接调用foo()
 // window.foo(); 
+
+function add(x, y, f) {
+  alert('add' + f(x) + f(y));
+  return f(x) + f(y);
+}
+
+
+function pow(x) {
+  return x * x;
+}
+
+function maptest() {
+  var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var results = arr.map(pow); // [1, 4, 9, 16, 25, 36, 49, 64, 81]
+  console.log(results);
+  alert(results);
+}
